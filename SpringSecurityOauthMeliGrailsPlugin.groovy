b947b94c4b09d857badcdf6c393c2e0c7efc229d
Fernando Niwes <fernando@soluciones3f.com.ar>
2015-03-24 12:59:52 -0300

    Project meta information

--- SpringSecurityOauthMeliGrailsPlugin.groovy
+++ SpringSecurityOauthMeliGrailsPlugin.groovy
@@ -30,9 +30,7 @@ Integrate [MercadoLibre|http://www.mercadolibre.com] to [Spring Security OAuth p
 '''
 
     // URL to the plugin's documentation
-    def documentation = "http://grails.org/plugin/spring-security-oauth-meli"
-
-    // Extra (optional) plugin metadata
+    def documentation = "https://github.com/fniwes/grails-spring-security-oauth-meli"
 
     // License: one of 'APACHE', 'GPL2', 'GPL3'
     def license = "APACHE"
@@ -40,43 +38,10 @@ Integrate [MercadoLibre|http://www.mercadolibre.com] to [Spring Security OAuth p
     // Details of company behind the plugin (if there is one)
     def organization = [ name: "Soluciones 3f SRL", url: "http://www.soluciones3f.com.ar/" ]
 
-    // Any additional developers beyond the author specified above.
-    def developers = [ [ name: "Fernando Aiel Niwes Naufal", email: "fernando@soluciones3f.com.ar" ]]
-
     // Location of the plugin's issue tracker.
-//    def issueManagement = [ system: "JIRA", url: "http://jira.grails.org/browse/GPMYPLUGIN" ]
+    def issueManagement = [ system: "GITHUB", url: "https://github.com/fniwes/grails-spring-security-oauth-meli/issues" ]
 
     // Online location of the plugin's browseable source code.
-//    def scm = [ url: "http://svn.codehaus.org/grails-plugins/" ]
+    def scm = [ url: "https://github.com/fniwes/grails-spring-security-oauth-meli" ]
 
-    def doWithWebDescriptor = { xml ->
-        // TODO Implement additions to web.xml (optional), this event occurs before
-    }
-
-    def doWithSpring = {
-        // TODO Implement runtime spring config (optional)
-    }
-
-    def doWithDynamicMethods = { ctx ->
-        // TODO Implement registering dynamic methods to classes (optional)
-    }
-
-    def doWithApplicationContext = { ctx ->
-        // TODO Implement post initialization spring config (optional)
-    }
-
-    def onChange = { event ->
-        // TODO Implement code that is executed when any artefact that this plugin is
-        // watching is modified and reloaded. The event contains: event.source,
-        // event.application, event.manager, event.ctx, and event.plugin.
-    }
-
-    def onConfigChange = { event ->
-        // TODO Implement code that is executed when the project configuration changes.
-        // The event is the same as for 'onChange'.
-    }
-
-    def onShutdown = { event ->
-        // TODO Implement code that is executed when the application shuts down (optional)
-    }
 }
